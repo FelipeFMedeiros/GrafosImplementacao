0a1242bbafc3bc18a8aa7b9febe404d94764ccaf
ismaelsantos1 <ismaelshenrique11@gmail.com>
2025-03-17 12:55:31 -0300

    Nicolas incluiu função para verificar se é regular

--- Grafo.py
+++ Grafo.py
@@ -31,8 +31,14 @@ class Grafo:
             self.arestas[v1] = [(v, p) for v, p in self.arestas[v1] if v != v2]
             self.arestas[v2] = [(v, p) for v, p in self.arestas[v2] if v != v1]
 
-    def eh_adjacente(self, v1, v2):
+    def a_eh_Adjacente(self, a1, a2):
+        v1, u1 = a1 #Primeira aresta (v1--u1)
+        v2, u2 = a2 #Segunda aresta (v2--u2)
+        return v1 in (v2,u2) or u1 in (v2,u2)
+
+    def v_eh_adjacente(self, v1, v2):
         return any(v2 == vizinho for vizinho, _ in self.arestas.get(v1, []))
+
 
     def lista_adjacencia(self, v):
         return self.arestas.get(v, [])
@@ -40,6 +46,12 @@ class Grafo:
     def grau(self, v):
         return len(self.arestas.get(v, []))
 
+    def eh_regular(self):
+        grau_ini = self.grau(next(iter(self.vertices)))  # salva o grau do primeiro vértice do grafo
+        for v in self.vertices:  # verifica o grau do resto dos vértices do grafo
+            if self.grau(v) != grau_ini:  # compara se os outros graus com o do primeiro
+                return False  # retorna False se algum grau for diferente
+        return True  # retorna True se todos os graus forem iguais
 
 # Exemplo de uso
 g = Grafo()
@@ -49,5 +61,9 @@ g.adicionar_vertice(3)
 g.adicionar_aresta(1, 2, 5)
 g.adicionar_aresta(2, 3, 7)
 g.imprimir()
-print(g.eh_adjacente(1,2))
+
+print(g.a_eh_Adjacente((1, 2), (2, 3)))  # True
+print(g.a_eh_Adjacente((1, 2), (3, 4)))  # False
+print(g.v_eh_adjacente(1,2))
 print("Grau do vértice 2:", g.grau(2))
+print('Lista de de Adjacência de um vertíce: ', g.lista_adjacencia(1))
